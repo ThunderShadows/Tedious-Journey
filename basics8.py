dab501c3e25c207f25843b36ca2b70180a0b5d5b
S G Sumanth <sumanthusha01@gmail.com>
2024-11-25 22:30:17 +0530

    Initial Commit12

--- basics8.py
+++ basics8.py
@@ -1,4 +1,5 @@
 import module as md
+import random as rd
 import ecommerce.shipping as eship
 
 value = int(input("Enter the number: "))
@@ -10,3 +11,11 @@ result1=int(md.max(number))
 print(f"The maximum value in the list is {result1}")
 
 eship.calc_shipping()
+
+captains = ["Jack Sparrow", "Will Turner", "Elizabeth Swann"]
+true_captain = rd.choice(captains)
+print(true_captain)
+
+first_value=rd.randint(1,6)
+second_value=rd.randint(1,6)
+print(first_value,second_value)
