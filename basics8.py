46e42e5e0440828642110328fb1ac8b508c4298c
S G Sumanth <sumanthusha01@gmail.com>
2024-11-25 21:39:59 +0530

    Initial Commit9

---
+++ basics8.py
@@ -0,0 +1,5 @@
+import module as md
+
+value = int(input("Enter the number: "))
+result = int(md.convert_temperature(value))
+print(f"The temeperatue is {result}")
---
+++ module.py
@@ -0,0 +1,7 @@
+def convert_temperature(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def convert_temperature(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
