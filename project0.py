6ea465cd648f75aec4b0c025b10bbe8d1215c813
S G Sumanth <134628185+ThunderShadows@users.noreply.github.com>
2024-11-28 23:33:34 +0530

    Delete project0.py

--- project0.py
+++
@@ -1,7 +0,0 @@
-# Python Automation project Using Excel and openpyxl library/package
-import openpyxl as xl
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
-cell = sheet['A1']
-cell = sheet.cell(1,1)
-print(cell.value)
