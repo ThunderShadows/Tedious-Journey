dd906e252f8fc7df3bbfba05c5456140b10ec49e
S G Sumanth <sumanthusha01@gmail.com>
2024-12-01 23:49:17 +0530

    DSA commit2

--- arrays1.py
+++ arrays1.py
@@ -1,3 +1,16 @@
+#Given an array arr[]. Push all the zeros of the given array to the right end of the array while maintaining the order of non-zero elements. Do the mentioned change in the array in place.
+
+# Examples:
+
+# Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+# Output: [1, 2, 4, 3, 5, 0, 0, 0]
+
+# Input: arr[] = [10, 20, 30]
+# Output: [10, 20, 30]
+
+# Input: arr[] = [0, 0]
+# Output: [0, 0]
+
 class Solution:
     def pushZerosToEnd(arr):
         count = 0
