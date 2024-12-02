9f348569880e196e2e3e5ff74e1c72fbd582bbec
S G Sumanth <sumanthusha01@gmail.com>
2024-12-02 23:23:35 +0530

    DSA commit3

--- arrays2.py
+++ arrays2.py
@@ -1,3 +1,16 @@
+# Given an array of integers arr, reverse it in-place.
+# Examples:
+
+# Input: arr = [1, 4, 3, 2, 6, 5]
+# Output: [5, 6, 2, 3, 4, 1]
+
+# Input: arr = [4, 5, 2]
+# Output: [2, 5, 4]
+
+# Input: arr = [1]
+# Output: [1]
+
+# Time Complexity: O(n) and Space Complexity: O(1)
 class Solution:
     def reverseArray(arr):
         left = 0
