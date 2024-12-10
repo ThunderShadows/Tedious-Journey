13edc3e8e5aef305100e6a77874f14f6699421b1
S G Sumanth <sumanthusha01@gmail.com>
2024-12-10 20:22:01 +0530

    DSA Commit12

---
+++ arrays8.py
@@ -0,0 +1,17 @@
+# Given an array arr[] denoting heights of N towers and a positive integer K.
+
+# For each tower, you must perform exactly one of the following operations exactly once.
+
+# Increase the height of the tower by K
+# Decrease the height of the tower by K
+# Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+
+# Examples :
+
+# Input: k = 2, arr[] = {1, 5, 8, 10}
+# Output: 5
+
+# Input: k = 3, arr[] = {3, 9, 12, 16, 20}
+# Output: 11
+
+# Time Complexity: O(n) and Space Complexity: O(1)
