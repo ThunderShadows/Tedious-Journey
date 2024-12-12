7376841e66a2fb54a310ec84a6dfe7a6e5975f7d
S G Sumanth <sumanthusha01@gmail.com>
2024-12-12 23:44:03 +0530

    DSA commit13

---
+++ arrays9.py
@@ -0,0 +1,16 @@
+# Given an array arr[], the task is to find the subarray that has the maximum sum and return its sum.
+
+# Examples:
+
+# Input: arr[] = {2, 3, -8, 7, -1, 2, 3}
+# Output: 11
+
+# Input: arr[] = {-2, -4}
+# Output: -2
+
+# Input: arr[] = {5, 4, 1, 7, 8}
+# Output: 25
+
+# Time Complexity: O(n) and Space Complexity: O(1)
+
+https://github.com/ThunderShadows/Tedious-Journey.git
