9e585dc133712f4cadc6ddb0465dd805a02c7b50
S G Sumanth <sumanthusha01@gmail.com>
2024-12-14 23:09:29 +0530

    DSA Commit13

--- arrays9.py
+++ arrays9.py
@@ -26,3 +26,26 @@ def maxSubarraySum(arr):
 
 arr = [2, 3, -8, 7, -1, 2, 3]
 print(maxSubarraySum(arr))
+
+# # Python Program to find the maximum subarray sum using nested loops
+
+# # Function to find the sum of subarray with maximum sum
+# def maxSubarraySum(arr):
+#     res = arr[0]
+  
+#     # Outer loop for starting point of subarray
+#     for i in range(len(arr)):
+#         currSum = 0
+      
+#         # Inner loop for ending point of subarray
+#         for j in range(i, len(arr)):
+#             currSum = currSum + arr[j]
+          
+#             # Update res if currSum is greater than res
+#             res = max(res, currSum)
+          
+#     return res
+
+# if __name__ == "__main__":
+#     arr = [2, 3, -8, 7, -1, 2, 3]
+#     print(maxSubarraySum(arr))
